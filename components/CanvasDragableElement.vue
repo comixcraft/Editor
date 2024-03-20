fe3d448ce3b3431a0da8b10326a5897d63c9da41
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-03-20 11:03:34 +0100

    Add bin icon to CanvasDragableElement
    I added an icon with a bin svg that is render regarding if the element is active or not

--- components/CanvasDragableElement.vue
+++ components/CanvasDragableElement.vue
@@ -7,11 +7,22 @@
         url: String,
     })
 
+    let elementActive = false;
+
 </script>
 
 <template>
-        <DraggableResizable :w="w" :h="h" :parent="true">
+        <DraggableResizable 
+        :x="150"
+        :y="150"
+        :w="w" 
+        :h="h" 
+        :parent="true" 
+        :class-name-active="'elementActive'" 
+        @activated="function() {elementActive = !elementActive}"
+        @deactivated="function() {elementActive = !elementActive}">
             <img :src="url" :alt="altText">
+            <img v-if="elementActive" src="/assets/bin.svg" alt="bin icon" id="deleteIcon">
         </DraggableResizable>
 </template>
 
@@ -20,6 +31,23 @@
     img {
         width: 100%;
         height: 100%;
+    }
+
+    .elementActive {
+        border: 1px dashed red;
+    }
+
+    #deleteIcon {
+        content: "";
+        position: absolute;
+        top: 10px;
+        right: -16px;    
+        width: 32px;
+        height: 32px;
+        background-color: white;
+        background: no-repeat center/80%  url('/assets/bin.svg');
+        border: 2px solid red;
+        border-radius: 5px;
     }
 
 </style>
