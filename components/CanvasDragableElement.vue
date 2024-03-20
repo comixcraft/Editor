216e549d272e6f10c73231c047c3f6ad1a22eaa1
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-03-20 11:18:30 +0100

    add icons svg

--- components/CanvasDragableElement.vue
+++ components/CanvasDragableElement.vue
@@ -22,7 +22,9 @@
         @activated="function() {elementActive = !elementActive}"
         @deactivated="function() {elementActive = !elementActive}">
             <img :src="url" :alt="altText">
-            <img v-if="elementActive" src="/assets/bin.svg" alt="bin icon" id="deleteIcon">
+            <img v-if="elementActive" src="/assets/bin.svg" alt="bin icon" class="icon" id="bin">
+            <img v-if="elementActive" src="/assets/bin.svg" alt="bin icon" class="icon" id="up-arrow">
+            <img v-if="elementActive" src="/assets/bin.svg" alt="bin icon" class="icon" id="dow-arrow">
         </DraggableResizable>
 </template>
 
@@ -37,17 +39,29 @@
         border: 1px dashed red;
     }
 
-    #deleteIcon {
-        content: "";
+    .icon {
         position: absolute;
-        top: 10px;
-        right: -16px;    
         width: 32px;
         height: 32px;
         background-color: white;
-        background: no-repeat center/80%  url('/assets/bin.svg');
         border: 2px solid red;
         border-radius: 5px;
+        right: -16px;   
+    }
+
+    #bin {
+        top: 10px;
+        background: no-repeat center/80%  url('/assets/bin.svg');
+    }
+
+    #up-arrow {
+        top: 52px;
+        background: no-repeat center/80%  url('/assets/');
+    }
+
+    #down-arrow {
+        top: 94px;
+        background: no-repeat center/80%  url('/assets/down-arrow.svg');
     }
 
 </style>
