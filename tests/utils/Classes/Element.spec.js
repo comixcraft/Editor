026bdd333e714574396f636f4c8027dcbebe05c5
Vanessa <47862956+Vanessi-K@users.noreply.github.com>
2024-04-03 11:51:55 +0200

    CXC-291: vitest test example
    Create a simple test example for the ElementDS class; to showcase how tests work.

---
+++ tests/utils/Classes/Element.spec.js
@@ -0,0 +1,30 @@
+import {describe, it, expect} from 'vitest';
+import ElementDS from "~/utils/Classes/Element.js";
+describe('Element', () => {
+    it('creates an element with passed and default properties', () => {
+        const sut = new ElementDS(0.2, 0.5, 'Image description', 'example.png');
+
+        const currentSutState = sut.currentState();
+
+        expect(currentSutState).hasOwnProperty('id');
+        expect(currentSutState).hasOwnProperty('z');
+        expect(currentSutState).hasOwnProperty('pos');
+        expect(currentSutState).hasOwnProperty('isFocused');
+        expect(currentSutState).hasOwnProperty('isMirrored');
+        expect(currentSutState).hasOwnProperty('rotation');
+        expect(currentSutState).hasOwnProperty('width');
+        expect(currentSutState).hasOwnProperty('height');
+        expect(currentSutState).hasOwnProperty('src');
+        expect(currentSutState).hasOwnProperty('alt');
+
+        expect(currentSutState.id).toBe(1);
+        expect(currentSutState.z).toBe(1);
+        expect(currentSutState.isFocused).toBe(false);
+        expect(currentSutState.isMirrored).toBe(false);
+        expect(currentSutState.rotation).toBe(0);
+        expect(currentSutState.width).toBe(0.2);
+        expect(currentSutState.height).toBe(0.5);
+        expect(currentSutState.src).toBe('example.png');
+        expect(currentSutState.alt).toBe('Image description');
+    });
+});
