29286a9655a81483a82c7f87b67bfab2ef3d790c
Vanessa <47862956+Vanessi-K@users.noreply.github.com>
2024-04-03 21:51:00 +0200

    CXC-291: testing setup (#6)
    * CXC-291: vitest setup
    
    Add a vitest setup and nuxt-test-utils to the project
    
    * CXC-291: vitest test example
    
    Create a simple test example for the ElementDS class; to showcase how tests work.
    
    * CXC-291: update README
    
    Added commands to run the tests to the README

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import {fileURLToPath} from 'node:url';
+import {defineVitestConfig} from '@nuxt/test-utils/config';
+import {resolve} from 'node:path';
+
+export default defineVitestConfig({
+    test: {
+        environment: 'nuxt',
+        environmentOptions: {
+            nuxt: {
+                rootDir: fileURLToPath(new URL('./', import.meta.url)),
+                overrides: {
+                    alias: {
+                        '~': resolve(fileURLToPath(new URL('./', import.meta.url))),
+                    },
+                },
+            },
+        },
+    }
+})
