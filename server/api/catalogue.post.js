ffa1b3f36f4d32424080ae2f77b0b66618bb78ef
Vanessa <47862956+Vanessi-K@users.noreply.github.com>
2024-04-04 14:10:37 +0200

    CXC-165: adjusted catalogue endpoint to be a post and handle different search possibilities.

---
+++ server/api/catalogue.post.js
@@ -0,0 +1,74 @@
+import {catalogue} from "~/server/assets/comixplain-catalogue.js";
+
+const whitelistCategories = {
+    'templates': 'Templates',
+    'characters': 'Characters',
+    'speech-bubbles': 'Speech Bubbles',
+    'scenes': 'Scenes',
+    'annotation-elements': 'Annotation Elements',
+}
+
+const whitelistSubCategories = {
+    'frames': 'Frames',
+    'facial-expressions': 'Facial Expressions',
+    'single': 'Single',
+    'multiple': 'Multiple',
+    'full-bubbles': 'Full Bubbles',
+    'half-bubbles': 'Half Bubbles',
+    'background': 'Background',
+    'items': 'Items',
+    'arrows': 'Arrows',
+    'circles': 'Circles',
+    'rectangles': 'Rectangles',
+    'lines': 'Lines',
+    'others': 'Others',
+}
+
+export default defineEventHandler(async (event) => {
+    let catalogueAssets = [];
+
+    const body = await readBody(event)
+
+    // Category
+    let categories = body.category;
+    if (!Array.isArray(categories)) {
+        categories = [categories];
+    }
+
+    categories = categories.map((c) => whitelistCategories[c] ? whitelistCategories[c] : null);
+    categories = categories.filter((c) => c !== null);
+
+    let categoryKeys = [];
+    if (categories.length > 0) {
+        categoryKeys = categories;
+    } else {
+        categoryKeys = Object.values(whitelistCategories);
+    }
+
+    // SubCategory
+    let subCategories = body.subCategory;
+    if (!Array.isArray(subCategories)) {
+        subCategories = [subCategories];
+    }
+
+    subCategories = subCategories.map((c) => whitelistSubCategories[c] ? whitelistSubCategories[c] : null);
+    subCategories = subCategories.filter((c) => c !== null);
+
+    let subCategoryKeys = [];
+    if (subCategories.length > 0) {
+        subCategoryKeys = subCategories;
+    } else {
+        subCategoryKeys = Object.values(whitelistSubCategories);
+    }
+
+    for (const categoryKey of categoryKeys) {
+        for (const [subCategoryKey, subCategoryValue] of Object.entries(catalogue[categoryKey])) {
+            if (subCategoryKeys.includes(subCategoryKey)) {
+                console.log("subCategoryKey",subCategoryKey);
+                catalogueAssets = catalogueAssets.concat(subCategoryValue.assets);
+            }
+        }
+    }
+
+    return catalogueAssets;
+})
