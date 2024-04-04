82b9556dd26c4217e4acb9f5c389d575a701f6c0
Esitsolf <esitsolf@hotmail.fr>
2024-04-04 13:04:05 +0200

    Implement the download functionality

--- components/PreviewCanvas.vue
+++ components/PreviewCanvas.vue
@@ -1,16 +1,56 @@
 <script setup>
+import {onMounted, watch} from 'vue';
 function closePreview() {
     document.querySelector(".previewContainer").classList.add("hide");
 }
+
+function displayPreview() {
+    const canvas = document.querySelector('.previewCanvas');
+    canvas.width = 300;
+    canvas.height = 500;
+
+    let context = canvas.getContext('2d');
+    // Drawing a circle
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+    var radius = 70;
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'rgba(0,200,0, .7)';
+    context.fill();
+    context.lineWidth = 2;
+    context.strokeStyle = 'black';
+    context.stroke();
+
+
+    elementsInCanvas.value.forEach((element, key) => {
+        console.log(element.currentState())
+    })
+
+}
+
+function download() {
+    const canvas = document.querySelector('.previewCanvas');
+    const dataURL = canvas.toDataURL("image/png");
+
+    // Create an link and click it to download the png
+    const a = document.createElement('a');
+    a.href = dataURL;
+    a.download = 'test.png';
+    a.click();
+}
+
+watch(elementsCounter, () => {displayPreview()});
+onMounted(() => displayPreview());
+
 </script>
 
 <template>
     <div class="previewContainer hide">
         <button class="closeBtn" @click="closePreview">X</button>
         <p>Preview</p>
-        <canvas class="previewCanvas"></canvas>
-        <button>download</button>
-
+        <canvas class="previewCanvas"> </canvas>
+        <button @click="download">download</button>
     </div>
 </template>
 
@@ -34,10 +74,14 @@ function closePreview() {
     align-items: center;
 }
 
+.hide {
+    display: none;
+}
+
 .previewCanvas {
     border: 1px solid black;
     width: 100%;
-    height: 100%;
+    height: auto;    
 }
 
 .closeBtn {
@@ -53,7 +97,15 @@ function closePreview() {
     color: white;
 }
 
-p{
+p {
     margin: 0;
+}
+
+@media (min-width: 768px) {
+    .previewCanvas {
+        border: 1px solid black;
+        width: auto;
+        height: 100%; 
+    }  
 }
 </style>
