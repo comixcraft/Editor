c24ccc86f67b3d0d9847f17a7a669f50368c981a
moonjuko <cc211028@fhstp.ac.at>
2024-04-18 16:28:10 +0200

    CXC-307: Created catalog
    Created catalog overlay
    Added sub navigation to the overlay from CatalogNavigation components
    Added basic styling

---
+++ components/CatalogOverlay.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div v-if="show" class="overlay">
+            <div class="navigation">
+                <div class="category__description">
+                    <span class="edit-icon icon text-primary">{{ iconName }}</span>
+                    <div class="navigation__title h1">
+                        {{ title }}
+                    </div>
+                </div>
+                <div class="navigation__icon icon" @click="$emit('close')">close</div>
+            </div>
+            <div class="overlay__content">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps, defineEmits } from 'vue';
+
+    const props = defineProps({
+        iconName: { type: String, default: '' },
+        title: { type: String, default: '' },
+        show: { type: Boolean, default: false },
+    });
+
+    const emit = defineEmits(['close']);
+</script>
+
+<style lang="scss" scoped>
+    .navigation {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: $white;
+        height: 80px;
+        margin: 0;
+        color: $primary;
+        cursor: pointer;
+    }
+    .category__description {
+        display: flex;
+        gap: 10px;
+    }
+    .overlay {
+        position: fixed;
+        top: 5.6%;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        padding: 16px;
+        border-top-left-radius: $border-radius-xl;
+        border-top-right-radius: $border-radius-xl;
+        background-color: $white;
+    }
+
+    .icon {
+        color: $grey-70;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,15 @@
 <script setup>
+    let characterCatalogShow = ref(false);
+    let speechBubbleCatalogShow = ref(false);
+    let textCatalogShow = ref(false);
+    let shapesCatalogShow = ref(false);
+    let sceneCatalogShow = ref(false);
+    let allAssetsCatalogShow = ref(false);
+
+    let subCharCatalogShow = ref(false);
+
+    const characterButtons = [{ label: 'Single' }, { label: 'Multiple' }, { label: 'Facial Expressions' }];
+
     // Values should come from the template chosen before opening the editor
     const canvasWidth = ref(450);
     const canvasHeight = ref(750);
@@ -28,21 +39,68 @@
 </script>
 
 <template>
-    <div class="container">
-        <WrapperCanvas></WrapperCanvas>
-        <CatalogContainer :assets="catalogElements"></CatalogContainer>
-    </div>
+    <div class="editor">
+        <div class="container">
+            <WrapperCanvas></WrapperCanvas>
+            <CatalogContainer :assets="catalogElements"></CatalogContainer>
+        </div>
 
-    <button>
-        <NuxtLink
-            :to="{
-                name: 'export',
-                path: '/export',
-                query: { width: canvasWidth, height: canvasHeight },
-            }"
-            >See Preview
-        </NuxtLink>
-    </button>
+        <button @click="characterCatalogShow = true">Characters</button>
+        <button @click="speechBubbleCatalogShow = true">Speech Bubble</button>
+        <button @click="textCatalogShow = true">Text</button>
+        <button @click="shapesCatalogShow = true">Shapes</button>
+        <button @click="sceneCatalogShow = true">Scenes</button>
+        <button @click="allAssetsCatalogShow = true">All Assets</button>
+
+        <CatalogOverlay
+            iconName="add_reaction"
+            title="Characters"
+            :show="characterCatalogShow"
+            @close="characterCatalogShow = false"
+        >
+            <CatalogNavigation
+                :show="characterCatalogShow"
+                :buttons="characterButtons"
+                @buttonClick="subCharCatalogShow"
+            ></CatalogNavigation>
+        </CatalogOverlay>
+        <CatalogOverlay
+            iconName="chat_bubble"
+            title="Speech Bubble"
+            :show="speechBubbleCatalogShow"
+            @close="speechBubbleCatalogShow = false"
+        >
+            This is Speech Bubble
+        </CatalogOverlay>
+        <CatalogOverlay iconName="sticky_note" title="Text" :show="textCatalogShow" @close="textCatalogShow = false">
+            This is Text
+        </CatalogOverlay>
+        <CatalogOverlay iconName="category" title="Shapes" :show="shapesCatalogShow" @close="shapesCatalogShow = false">
+            This is Shapes
+        </CatalogOverlay>
+        <CatalogOverlay iconName="scene" title="Scene" :show="sceneCatalogShow" @close="sceneCatalogShow = false">
+            This is Scene
+        </CatalogOverlay>
+        <CatalogOverlay
+            iconName="image_search"
+            title="All Assets"
+            :show="allAssetsCatalogShow"
+            @close="allAssetsCatalogShow = false"
+        >
+            This is All Assets
+        </CatalogOverlay>
+
+        <button>
+            <NuxtLink
+                :to="{
+                    name: 'export',
+                    path: '/export',
+                    query: { width: canvasWidth, height: canvasHeight },
+                }"
+                >See Preview
+            </NuxtLink>
+        </button>
+    </div>
 </template>
 
 <style scoped>
@@ -50,5 +108,9 @@
         display: flex;
         justify-content: space-evenly;
         align-items: center;
+    }
+
+    .editor {
+        background-color: #6360f4;
     }
 </style>
