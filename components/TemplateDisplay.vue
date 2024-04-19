ae3fbe1ecaf9a127e1a35a80340e57d7885d85c5
Vanessa <47862956+Vanessi-K@users.noreply.github.com>
2024-04-19 11:51:39 +0200

    CXC-169: provide-standard-templates: show template options on index page

---
+++ components/TemplateDisplay.vue
@@ -0,0 +1,45 @@
+<script setup>
+    const props = defineProps({
+        title: {
+            type: String,
+            default: '',
+        },
+        preview: {
+            type: String,
+            default: '',
+        },
+        config: {
+            type: Object,
+            default: () => ({}),
+        },
+    });
+</script>
+
+<template>
+    <div class="template">
+        <p class="h5 template__title">{{ title }}</p>
+        <img class="template__preview" :src="preview" :alt="title" />
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .template {
+        display: flex;
+        flex-direction: column;
+        padding: $spacer-3 $spacer-4;
+        border: $border-width-lg solid $grey-100;
+        border-radius: $border-radius;
+
+        &__preview {
+            height: 20vh;
+            max-height: 250px;
+            min-height: 100px;
+            width: auto;
+        }
+
+        &__title {
+            color: $medium-grey-100;
+            margin-bottom: $spacer-1;
+        }
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,94 @@
+<script setup>
+    let panelOptions = reactive([
+        {
+            title: 'Single',
+            preview: '/frames/preview/Preview_Single.png',
+            config: { height: 1084, panels: [{ width: 759, border: '/frames/Frame-Single.png' }] },
+        },
+        {
+            title: 'Double',
+            preview: '/frames/preview/Preview_Double.png',
+            config: { height: 1084, panels: [{ width: 1552, border: '/frames/Frame-Double.png' }] },
+        },
+        {
+            title: 'Triple',
+            preview: '/frames/preview/Preview_Triple.png',
+            config: { height: 1084, panels: [{ width: 2327, border: '/frames/Frame-Triple.png' }] },
+        },
+    ]);
+
+    let stripOptions = reactive([
+        {
+            title: 'Three Single',
+            preview: '/frames/preview/Preview_Single-Single-Single.png',
+            config: {
+                height: 1084,
+                panels: [
+                    { width: 759, border: '/frames/Frame-Single.png' },
+                    { width: 759, border: '/frames/Frame-Single.png' },
+                    { width: 759, border: '/frames/Frame-Single.png' },
+                ],
+            },
+        },
+        {
+            title: 'Double and Single',
+            preview: '/frames/preview/Preview_Double-Single.png',
+            config: {
+                height: 1084,
+                panels: [
+                    { width: 1552, border: '/frames/Frame-Double.png' },
+                    { width: 759, border: '/frames/Frame-Single.png' },
+                ],
+            },
+        },
+        {
+            title: 'Single and Double',
+            preview: '/frames/preview/Preview_Single-Double.png',
+            config: {
+                height: 1084,
+                panels: [
+                    { width: 1552, border: '/frames/Frame-Double.png' },
+                    { width: 759, border: '/frames/Frame-Single.png' },
+                ],
+            },
+        },
+        {
+            title: 'Two Single',
+            preview: '/frames/preview/Preview_Single-Single.png',
+            config: {
+                height: 1084,
+                panels: [
+                    { width: 759, border: '/frames/Frame-Single.png' },
+                    { width: 759, border: '/frames/Frame-Single.png' },
+                ],
+            },
+        },
+    ]);
+
+    let selectedComicConfiguration = ref(null);
+</script>
+
+<template>
+    <div>
+        <div class="d-flex flex-wrap gap-3">
+            <TemplateDisplay
+                v-for="option in panelOptions"
+                :key="option.title"
+                :title="option.title"
+                :preview="option.preview"
+                :config="option.config"
+            />
+        </div>
+        <div class="d-flex flex-wrap gap-3">
+            <TemplateDisplay
+                v-for="option in stripOptions"
+                :key="option.title"
+                :title="option.title"
+                :preview="option.preview"
+                :config="option.config"
+            />
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss"></style>
