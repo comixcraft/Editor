78fe0e5cc4727e68b8d0e87333ff81f721529279
Esitsolf <esitsolf@hotmail.fr>
2024-04-19 15:43:12 +0200

    text working accordingly on the editor
    The input is now part of the wrapper.
    All the function related to it are also there.
    The text can be edited when clicking on a text element once the element is already selected.
    The text gets edited, displayed and modified in the element.

--- components/CanvasDraggableElement.vue
+++ components/CanvasDraggableElement.vue
@@ -1,129 +1,173 @@
 <script setup>
-    import ElementDS from '../utils/Classes/Element.js'
+import ElementDS from "../utils/Classes/Element.js";
+import { ref } from "vue";
 
-    const props = defineProps({
-        z: Number,
-        w: Number,
-        h: Number,
-        altText: String,
-        url: String,
-        eId: Number,
-        pos: Object,
-        isMirrored: Boolean,
-        type: Object
-    })
+const props = defineProps({
+  z: Number,
+  w: Number,
+  h: Number,
+  altText: String,
+  url: String,
+  eId: Number,
+  pos: Object,
+  isMirrored: Boolean,
+  type: Object,
+  startModifyText: Function,
+  resetClicks: Function,
+});
 
-    let elementActive = false;
-    let mirrored = ref(props.isMirrored);
-    let self = ref(null);
+let elementActive = false;
+let mirrored = ref(props.isMirrored);
+let self = ref(null);
 
-    function updatePosition(eId) {
-        // check what map entry correspond to id
-        let matchingIdEntry;
-        elementsInCanvas.value.forEach((value, key) => {
-            if (value.currentState().id === eId) matchingIdEntry = value;
-        })
-        if (!matchingIdEntry) {
-            console.log('Error in id passing for updatePosition function [CanvasDraggableElement:20]')
-            return;
-        }
-        // update position of element
-        matchingIdEntry.setPos({x: self.value.left, y: self.value.top})
+function updatePosition(eId) {
+  props.resetClicks();
+  // check what map entry correspond to id
+  let matchingIdEntry;
+  elementsInCanvas.value.forEach((value, key) => {
+    if (value.currentState().id === eId) matchingIdEntry = value;
+  });
+  if (!matchingIdEntry) {
+    console.log(
+      "Error in id passing for updatePosition function [CanvasDraggableElement:29]"
+    );
+    return;
+  }
+  // update position of element
+  matchingIdEntry.setPos({ x: self.value.left, y: self.value.top });
+}
+
+function resize(eId) {
+  // check what map entry correspond to id
+  let matchingIdEntry;
+  elementsInCanvas.value.forEach((value, key) => {
+    if (value.currentState().id === eId) matchingIdEntry = value;
+  });
+  if (!matchingIdEntry) {
+    console.log(
+      "Error in id passing for updatePosition function [CanvasDraggableElement:20]"
+    );
+    return;
+  }
+
+  matchingIdEntry.setPos({ x: self.value.left, y: self.value.top });
+  matchingIdEntry.setWidth(self.value.width);
+  matchingIdEntry.setHeight(self.value.height);
+}
+
+function updateMirroring(eId) {
+  // mirror the image on editor
+  this.mirrored = !this.mirrored;
+  // recover the element
+  let matchingIdEntry;
+  elementsInCanvas.value.forEach((value, key) => {
+    if (value.currentState().id === eId) matchingIdEntry = value;
+  });
+  if (!matchingIdEntry) {
+    console.log(
+      "Error in id passing for updatePosition function [CanvasDraggableElement:20]"
+    );
+    return;
+  }
+  // update isMirrored of element
+  matchingIdEntry.setIsMirrored(this.mirrored);
+}
+
+function startModifyText(eId) {
+  if (elementActive) {
+    let matchingIdEntry;
+    elementsInCanvas.value.forEach((value, key) => {
+      if (value.currentState().id === eId) matchingIdEntry = value;
+    });
+    if (!matchingIdEntry) {
+      console.log(
+        "Error in id passing for startModifyText function [CanvasDraggableElement:76]"
+      );
+      return;
     }
-
-    function resize(eId) {
-        // check what map entry correspond to id
-        let matchingIdEntry;
-        elementsInCanvas.value.forEach((value, key) => {
-            if (value.currentState().id === eId) matchingIdEntry = value;
-        })
-        if (!matchingIdEntry) {
-            console.log('Error in id passing for updatePosition function [CanvasDraggableElement:20]')
-            return;
-        }
-
-        matchingIdEntry.setPos({x: self.value.left, y: self.value.top});
-        matchingIdEntry.setWidth(self.value.width);
-        matchingIdEntry.setHeight(self.value.height);
-    }
-
-    function updateMirroring(eId) {
-        // mirror the image on editor
-        this.mirrored = !this.mirrored;
-        // recover the element
-        let matchingIdEntry;
-        elementsInCanvas.value.forEach((value, key) => {
-            if (value.currentState().id === eId) matchingIdEntry = value;
-        })
-        if (!matchingIdEntry) {
-            console.log('Error in id passing for updatePosition function [CanvasDraggableElement:20]')
-            return;
-        }
-        // update isMirrored of element
-        matchingIdEntry.setIsMirrored(this.mirrored);
-    }
-    
-    function modifyText(eId) {
-        // modify text of element
-        let matchingIdEntry;
-        elementsInCanvas.value.forEach((value, key) => {
-            if (value.currentState().id === props.eId) matchingIdEntry = value;
-        })
-        if (!matchingIdEntry) {
-            console.log('Error in id passing for updatePosition function [CanvasDraggableElement:20]')
-            return;
-        }
-        let newText = prompt('Enter new text');
-        matchingIdEntry.currentState().type.setContent(newText);
-        console.log(matchingIdEntry.currentState());
-    }
+    props.startModifyText(true, matchingIdEntry);
+  }
+}
 </script>
 
 <template>
-        <DraggableResizable
-        :z="z"
-        :w="w" 
-        :h="h"
-        :x="pos.currPos().x"
-        :y="pos.currPos().y" 
-        :eId="eId"
-        :parent="true" 
-        class-name-active="elementActive"
-        :disableUserSelect="true"
-        ref="self"
-        @activated="function() {elementActive = !elementActive}"
-        @deactivated="function() {elementActive = !elementActive}"
-        @drag-stop="updatePosition(eId)"
-        @resize-stop="resize(eId)">
-            <EditionMenu 
-            v-if="elementActive"
-            @mirror-event="updateMirroring(eId)"
-            @delete-event="$emit('deleteEvent', z)"
-            />
+  <DraggableResizable
+    :z="z"
+    :w="w"
+    :h="h"
+    :x="pos.currPos().x"
+    :y="pos.currPos().y"
+    :eId="eId"
+    :parent="true"
+    class-name-active="elementActive"
+    :disableUserSelect="true"
+    ref="self"
+    @activated="elementActive = !elementActive"
+    @deactivated="elementActive = !elementActive"
+    @drag-stop="updatePosition(eId)"
+    @resize-stop="resize(eId)"
+  >
+    <EditionMenu
+      v-if="elementActive"
+      @mirror-event="updateMirroring(eId)"
+      @delete-event="$emit('deleteEvent', z)"
+    />
 
-            <p 
-            v-if="type.getName() == 'Text'"
-            @click="modifyText(eId)"
-            >
-                {{type.getContent()}}
-            </p>
-            
-            <img :src="url" :alt="altText" :class="{mirror : mirrored}" v-if="type.getName() == 'Asset'">
-        
-        </DraggableResizable>
+    <div
+      tabindex="-1"
+      class="textContainer"
+      v-if="type.getName() == 'Text'"
+      @click="startModifyText(eId)"
+    >
+      <p>
+        {{ type.getContent() }}
+      </p>
+    </div>
+
+    <img
+      :src="url"
+      :alt="altText"
+      :class="{ mirror: mirrored }"
+      v-if="type.getName() == 'Asset'"
+    />
+  </DraggableResizable>
 </template>
 
 <style scoped lang="scss">
-    img {
-        width: 100%;
-        height: 100%;
-    }
+img {
+  width: 100%;
+  height: 100%;
+}
 
-    .elementActive {
-        border: $border-width solid $info;
-    }
+.elementActive {
+  border: $border-width solid $info;
+}
 
-    .mirror {
-        transform: scaleX(-1);
-    }
+.mirror {
+  transform: scaleX(-1);
+}
+
+p {
+  width: 100%;
+  height: 100%;
+}
+
+textarea {
+  width: auto;
+  height: auto;
+  border: none;
+  padding: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+
+  &:focus-visible {
+    border: 1px solid red;
+  }
+}
+
+.textContainer {
+  width: 100%;
+  height: 100%;
+}
 </style>
--- components/WrapperCanvas.vue
+++ components/WrapperCanvas.vue
@@ -1,77 +1,128 @@
 <script setup>
+import ElementDS from "../utils/Classes/Element.js";
 
-import ElementDS from '../utils/Classes/Element.js'
+const textarea = ref(null);
+let clicks = ref(0);
+let modifyTextActive = ref(false);
+let placeholderText = ref("yeah!");
+let currentElement = ref(null);
 
 function showMap() {
-    console.clear()
-    elementsInCanvas.value.forEach((value, key) => {
-        console.log(`${key} is ${JSON.stringify(value.currentState())}. [instance of ElementDS: ${value instanceof ElementDS}]`)
-    })
+  console.clear();
+  elementsInCanvas.value.forEach((value, key) => {
+    console.log(
+      `${key} is ${JSON.stringify(value.currentState())}. [instance of ElementDS: ${value instanceof ElementDS}]`
+    );
+  });
 }
 
 function deleteElement(elId) {
-    // all z element
-    changeZIndex(elId)
-    // delete last element of map
-    elementsInCanvas.value.delete(elementsInCanvas.value.size)
+  // all z element
+  changeZIndex(elId);
+  // delete last element of map
+  elementsInCanvas.value.delete(elementsInCanvas.value.size);
 }
 
 function changeZIndex(z) {
-    // change all z index
-    if (z > elementsInCanvas.value.size - 1) return; // stop recursive call when reaching second to last element (last one will be deleted)
+  // change all z index
+  if (z > elementsInCanvas.value.size - 1) return; // stop recursive call when reaching second to last element (last one will be deleted)
 
-    let nextElement = elementsInCanvas.value.get(z + 1)
+  let nextElement = elementsInCanvas.value.get(z + 1);
 
-    nextElement.setZIndex(z) // change z-index of next element
-    elementsInCanvas.value.set(z, nextElement) // update map element with next element
-    z++; // increment z index
-    changeZIndex(z) // recursive
+  nextElement.setZIndex(z); // change z-index of next element
+  elementsInCanvas.value.set(z, nextElement); // update map element with next element
+  z++; // increment z index
+  changeZIndex(z); // recursive
 }
 
+function startModifyText(newValue, element) {
+  clicks.value++;
+  if (clicks.value === 2) {
+    modifyTextActive.value = newValue;
+    nextTick(() => {
+      currentElement.value = element;
+      textarea.value.focus();
+      placeholderText = element.currentState().type.getContent();
+    });
+  }
+}
+
+function stopModifyText() {
+  modifyTextActive.value = false;
+  clicks.value = 0;
+  currentElement.value.currentState().type.setContent(textarea.value.value);
+  textarea.value.value = "";
+}
+
+function resetClicks() {
+  clicks.value = 0;
+}
 </script>
 
 <template>
-    <button @click="showMap">show map</button>
-    <div class="wrapper" ref="container">
-      <CanvasDraggableElement v-for="[key, value] in elementsInCanvas"
-        @delete-event="deleteElement"
-        :key="value.currentState().id"
-        :eId="value.currentState().id"
-        :z="value.currentState().z"
-        :w="value.currentState().width"
-        :h="value.currentState().height"
-        :altText="value.currentState().name" 
-        :url="value.currentState().src"
-        :pos="value.currentState().pos"
-        :isMirrored="value.currentState().isMirrored"
-        :type="value.currentState().type"
+  <button @click="showMap">show map</button>
+  <div class="wrapper" ref="container">
+    <CanvasDraggableElement
+      v-for="[key, value] in elementsInCanvas"
+      @delete-event="deleteElement"
+      :key="value.currentState().id"
+      :eId="value.currentState().id"
+      :z="value.currentState().z"
+      :w="value.currentState().width"
+      :h="value.currentState().height"
+      :altText="value.currentState().name"
+      :url="value.currentState().src"
+      :pos="value.currentState().pos"
+      :isMirrored="value.currentState().isMirrored"
+      :type="value.currentState().type"
+      :startModifyText="startModifyText"
+      :resetClicks="resetClicks"
+    />
+    <div class="textarea-container" v-show="modifyTextActive">
+      <input
+        tabindex="0"
+        :placeholder="placeholderText"
+        ref="textarea"
+        @blur="stopModifyText"
       />
-      <!-- <CanvasDraggableElement v-for="element in elementsInCanvas" :w="element.width" :h="element.height" :z="1" :altText="element.name" :url="element.src"/> -->
     </div>
+    <!-- <CanvasDraggableElement v-for="element in elementsInCanvas" :w="element.width" :h="element.height" :z="1" :altText="element.name" :url="element.src"/> -->
+  </div>
 </template>
 
 <style>
-@import 'vue-draggable-resizable/style.css';
+@import "vue-draggable-resizable/style.css";
 
 .wrapper {
-    /* sould be defined dynamically once the value comes from template */
-    width: 450px;
-    height: 750px;
-    
-    border: 1px solid #000;
-    overflow: hidden;
-    display: grid;
+  /* sould be defined dynamically once the value comes from template */
+  width: 450px;
+  height: 750px;
+
+  border: 1px solid #000;
+  overflow: hidden;
+  display: grid;
 }
 
 .box {
-    margin-top: 2rem;
-    padding: 1rem;
-    outline: solid 1px #ccc;
+  margin-top: 2rem;
+  padding: 1rem;
+  outline: solid 1px #ccc;
 }
 
 .hide {
-    display: none;
+  display: none;
 }
 
-
+.textarea-container {
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+  background-color: rgba(184 184 184 / 0.5);
+}
 </style>
