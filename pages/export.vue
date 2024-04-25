1f1fe68094811def0c23547268fec2c01fc9a84e
hannahwagner1402 <92428401+hannahwagner1402@users.noreply.github.com>
2024-04-25 15:59:26 +0200

    Cxc 155 ensuring responsive design (#20)
    * Initial layout created and button added to index
    
    * remove console log
    
    * CXC-155: Added modal pop up
    opens and closes with test button
    
    * Mobile Layouts Done
    Following layouts are done: welcome page, editor, preview, layers, export
    Not yet responsive and swiping isnt implemented yet, but the layout for the test pages is finished.
    
    * Editor and welcome page fully responsive
    still need fine tuning, export page will follow soon once questions cleared up
    
    * CXC-155 Fully responsive
    
    with a few hiccups still, but working
    
    * CXC-155 renaming of editor page
    
    * CXC-155 Implementing layout with styling
    
    Not all pages are done, but the welcome Page is finished
    
    * CXC-155 Finished ensuring responsive layouts
    
    * CXC-155 Added changes from review
    
    * CXC-155 Added final changes from review

--- pages/export.vue
+++ pages/export.vue
@@ -1,11 +1,39 @@
 <template>
-    <div ref="previewCanvas" class="preview__container">
-        <button>
-            <NuxtLink to="/editor">Back</NuxtLink>
-        </button>
-        <p>Preview</p>
-        <canvas ref="canvasEl" class="preview__canvas"></canvas>
-        <button @click="download">download</button>
+    <div class="share">
+        <div class="share__top-nav">
+            <NuxtLink to="/editor" class="share__top-nav-item back-btn icon"> arrow_back </NuxtLink>
+            <div class="share__top-nav-item download-txt">Download Comic</div>
+        </div>
+        <div class="share__body">
+            <div class="share__input-group">
+                <label class="share__input-group-label" for="project-name">Project Name:</label>
+                <input
+                    class="share__input-group-input"
+                    type="text"
+                    id="project-name"
+                    placeholder="Enter project name"
+                />
+            </div>
+            <div class="share__input-group">
+                <label class="share__input-group-label" for="file-type">File Type:</label>
+                <select class="file-type-select">
+                    <option value="png">PNG</option>
+                </select>
+            </div>
+            <div class="share__input-group">
+                <label class="share__input-group-label" for="select-panels">Select Panels:</label>
+                <select class="file-type-select">
+                    <option value="1">All panels</option>
+                </select>
+            </div>
+            <div ref="previewCanvas" class="preview__container">
+                <h3>Preview:</h3>
+                <canvas ref="canvasEl" class="preview__canvas"></canvas>
+            </div>
+        </div>
+        <div class="share__confirm">
+            <button class="share__confirm-btn" @click="download">download</button>
+        </div>
     </div>
 </template>
 
@@ -89,43 +117,91 @@
     });
 </script>
 
-<style>
+<style scoped lang="scss">
+    .share__top-nav {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        background: linear-gradient(90deg, #6360f4 44.5%, #f460b7 100%);
+        height: 80px;
+        margin: 0;
+        color: white;
+    }
+
+    .share__body {
+        padding: $spacer-3;
+    }
+
+    .share__input-group {
+        margin-bottom: $spacer-4;
+    }
+
+    .share__input-group-label {
+        display: block;
+        margin-bottom: 5px;
+    }
+
+    .share__input-group-input,
+    .share__input-group-select {
+        padding: $spacer-2;
+        padding-right: $spacer-6;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+    }
+
+    .share__preview {
+        margin-top: $spacer-5;
+    }
+
+    .share__confirm {
+        position: absolute;
+        bottom: 10%;
+        right: 7%;
+        padding: $spacer-3;
+        z-index: 999;
+    }
+
+    .share__confirm-btn {
+        position: absolute;
+        bottom: 10%;
+        right: 7%;
+        padding: $spacer-3;
+        background-color: #f460b7;
+        z-index: 9999;
+    }
+
+    .share__top-nav-item {
+        padding: $spacer-3;
+        $font-size-phone: 3;
+        color: #eee;
+        text-decoration: none;
+    }
+
     .preview__container {
         z-index: 1000;
         justify-self: center;
         align-self: center;
-        width: 75vw;
-        height: 85vh;
-        position: absolute;
-        top: 7.5vh;
-        left: 12.5vw;
         background-color: white;
-        display: grid;
-        grid-template-rows: auto auto 1fr auto;
-        padding: 0.25rem;
-        gap: 0.25rem;
         justify-items: center;
         align-items: center;
     }
 
     .preview__canvas {
         border: 1px solid black;
-
         background-color: white;
         box-shadow: 3px 3px 3px 3px rgba(0, 0, 0, 0.75);
         width: 100%;
         height: auto;
     }
-
-    p {
-        margin: 0;
-    }
-
-    @media (min-width: 768px) {
+    @include media-breakpoint-up(m) {
         .preview__canvas {
             border: 1px solid black;
             width: auto;
             height: 100%;
+        }
+        .preview__container {
+            height: 25vh;
+            width: 25vw;
         }
     }
 </style>
