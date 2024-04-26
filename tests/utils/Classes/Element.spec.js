3494db7871c61d8ac885d52720d28f7b9786a214
Vanessa <47862956+Vanessi-K@users.noreply.github.com>
2024-04-26 17:03:59 +0200

    CXC-292: testing: crete tests for the Element class and add JSDoc

---
+++ tests/utils/Classes/Element.spec.js
@@ -0,0 +1,67 @@
+import { describe, expect, it } from 'vitest';
+import Element from '~/utils/Classes/Element.js';
+import Position from '~/utils/Classes/Position.js';
+
+describe('Element', () => {
+    it('creates an empty Element', () => {
+        const sut = new Element();
+
+        expect(sut.id).toBe(undefined);
+        expect(sut.z).toBe(0);
+        expect(sut.pos).toBeDefined();
+        expect(sut.pos.x).toBe(0);
+        expect(sut.pos.y).toBe(0);
+        expect(sut.isFocused).toBe(false);
+        expect(sut.isMirrored).toBe(false);
+        expect(sut.rotation).toBe(0);
+        expect(sut.width).toBe(0);
+        expect(sut.height).toBe(0);
+        expect(sut.src).toBe('undefined');
+        expect(sut.alt).toBe('unknown');
+        expect(sut.type).toBe(undefined);
+    });
+
+    it('creates an Element with passed properties', () => {
+        const sut = new Element(300, 400, 'alt', 'src');
+
+        expect(sut.id).toBe(undefined);
+        expect(sut.z).toBe(0);
+        expect(sut.pos).toBeDefined();
+        expect(sut.pos.x).toBe(0);
+        expect(sut.pos.y).toBe(0);
+        expect(sut.isFocused).toBe(false);
+        expect(sut.isMirrored).toBe(false);
+        expect(sut.rotation).toBe(0);
+        expect(sut.width).toBe(300);
+        expect(sut.height).toBe(400);
+        expect(sut.src).toBe('src');
+        expect(sut.alt).toBe('alt');
+        expect(sut.type).toBe(undefined);
+    });
+
+    it('sets the Element position', () => {
+        const sut = new Element();
+
+        sut.setPos({ x: 100, y: 200 });
+
+        expect(sut.pos.x).toBe(100);
+        expect(sut.pos.y).toBe(200);
+    });
+
+    it('returns a JSON representation of the Element', () => {
+        const sut = new Element();
+
+        expect(sut.currentState()).toStrictEqual({
+            id: undefined,
+            z: 0,
+            pos: new Position(0, 0),
+            isFocused: false,
+            isMirrored: false,
+            rotation: 0,
+            width: 0,
+            height: 0,
+            src: 'undefined',
+            alt: 'unknown',
+        });
+    });
+});
--- utils/Classes/Element.js
+++ utils/Classes/Element.js
@@ -1,25 +1,45 @@
 import Position from './Position.js';
 
 export default class ElementDS {
+    /** @type {String} */
     _id;
+    /** @type {Number} */
     _z = 0;
+    /** @type {Position} */
     _pos = new Position(0, 0);
+    /** @type {Boolean} */
     _isFocused = false;
+    /** @type {Boolean} */
     _isMirrored = false;
+    /** @type {Number} */
     _rotation = 0;
+    /** @type {Number} */
     _width;
+    /** @type {Number} */
     _height;
+    /** @type {String} */
     _src;
+    /** @type {String} */
     _alt;
+    /** @type {String} */
     _type;
 
-    constructor(width = 0, height = 0, alt = 'unknown', src = 'undefined') {
-        this._width = width;
-        this._height = height;
-        this._src = src;
-        this._alt = alt;
+    /**
+     * @param {Number} width
+     * @param {Number} height
+     * @param {String} alt
+     * @param {String} src
+     */
+    constructor(width = null, height = null, alt = null, src = null) {
+        this._width = width ?? 0;
+        this._height = height ?? 0;
+        this._src = src ?? 'undefined';
+        this._alt = alt ?? 'unknown';
     }
 
+    /**
+     * @returns {{id:String, z:Number, pos:Position, isFocused:Boolean, isMirrored:Boolean, rotation:Number, width:Number, height:Number, src:String, alt:String}
+     */
     currentState = () => {
         return {
             id: this._id,
@@ -35,75 +55,164 @@ export default class ElementDS {
         };
     };
 
-    // GETTERS
+    // GETTER
+    /** @returns {String} */
     get id() {
         return this._id;
     }
 
+    /** @returns {Number} */
     get z() {
         return this._z;
     }
 
+    /** @returns {Position} */
     get pos() {
         return this._pos;
     }
 
+    /** @returns {Boolean} */
     get isFocused() {
         return this._isFocused;
     }
 
+    /** @returns {Boolean} */
     get isMirrored() {
         return this._isMirrored;
     }
 
+    /** @returns {Number} */
     get rotation() {
         return this._rotation;
     }
 
+    /** @returns {Number} */
     get width() {
         return this._width;
     }
 
+    /** @returns {Number} */
     get height() {
         return this._height;
     }
 
+    /** @returns {String} */
     get src() {
         return this._src;
     }
 
+    /** @returns {String} */
     get alt() {
         return this._alt;
     }
 
+    /** @returns {String} */
     get type() {
         return this._type;
     }
 
+    // SETTER
+    /** @param {String} id */
+    set id(id) {
+        this._id = id;
+    }
+
+    /** @param {Number} z */
+    set z(z) {
+        this._z = z;
+    }
+
+    /** @param {Position} pos */
+    set pos(pos) {
+        this._pos = pos;
+    }
+
+    /** @param {Boolean} isFocused */
+    set isFocused(isFocused) {
+        this._isFocused = isFocused;
+    }
+
+    /** @param {Boolean} isMirrored */
+    set isMirrored(isMirrored) {
+        this._isMirrored = isMirrored;
+    }
+
+    /** @param {Number} rotation */
+    set rotation(rotation) {
+        this._rotation = rotation;
+    }
+
+    /** @param {Number} width */
+    set width(width) {
+        this._width = width;
+    }
+
+    /** @param {Number} height */
+    set height(height) {
+        this._height = height;
+    }
+
+    /** @param {String} src */
+    set src(src) {
+        this._src = src;
+    }
+
+    /** @param {String} alt */
+    set alt(alt) {
+        this._alt = alt;
+    }
+
+    /** @param {String} type */
+    set type(type) {
+        this._type = type;
+    }
+
+    // METHODS
+    /**
+     * @param {{x:Numer, y:Number}} obj
+     */
     setPos = (obj) => {
         this._pos.definePos(obj);
     };
 
-    setZIndex = (n) => {
-        this._z = n;
+    /**
+     * @param {Number} z
+     */
+    setZIndex = (z) => {
+        this._z = z;
     };
 
-    setId = (n) => {
-        this._id = n;
+    /**
+     * @param {Number} id
+     */
+    setId = (id) => {
+        this._id = id;
     };
 
-    setIsMirrored = (bool) => {
-        this._isMirrored = bool;
+    /**
+     * @param {Boolean} isMirrored
+     */
+    setIsMirrored = (isMirrored) => {
+        this._isMirrored = isMirrored;
     };
 
-    set focused(bool) {
-        this._isFocused = bool;
+    /**
+     * @param {Boolean} isFocused
+     */
+    set focused(isFocused) {
+        this._isFocused = isFocused;
     }
 
+    /**
+     * @param {Number} width
+     */
     setWidth = (width) => {
         this._width = width;
     };
 
+    /**
+     * @param {Number} height
+     */
     setHeight = (height) => {
         this._height = height;
     };
