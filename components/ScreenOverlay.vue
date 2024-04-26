6dce78c5807712985d78228d77c2fb6c9cf91732
Alica Holla <alica.holla@gmail.com>
2024-04-26 15:17:58 +0200

    Adjust styling of ScreenOverlay
    - padding on sides of the navbar added
    - cursor on hover on icon added

--- components/ScreenOverlay.vue
+++ components/ScreenOverlay.vue
@@ -2,7 +2,7 @@
     <div v-if="show" class="overlay">
         <div class="navigation">
             <div class="navigation__title">{{ title }}</div>
-            <div class="navigation__icon icon" @click="$emit('close')">close</div>
+            <button class="navigation__icon icon" @click="$emit('close')">close</button>
         </div>
         <div class="overlay__content"><slot></slot></div>
     </div>
@@ -22,16 +22,29 @@
         justify-content: space-between;
         background: linear-gradient(90deg, #6360f4 44.5%, #f460b7 100%);
         height: 80px;
-        margin: 0;
-        padding: 16px;
+        padding: 0 $spacer-5;
         color: white;
     }
+
     .overlay {
         height: 100vh;
-        width: 100vw;
         position: fixed;
         z-index: 999999;
         top: 0;
         left: 0;
+    }
+
+    .navigation__icon {
+        border: none;
+        height: 40px;
+        width: 40px;
+        text-align: center;
+        border-radius: $border-radius;
+        vertical-align: middle;
+        background-color: transparent;
+        color: white;
+    }
+    .navigation__icon:hover {
+        cursor: pointer;
     }
 </style>
