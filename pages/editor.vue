aa95726fa4cbafa5817b366d687647d1cb7236da
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-05-08 17:12:46 +0200

    fix: manual merge for the editor file

--- pages/editor.vue
+++ pages/editor.vue
@@ -9,7 +9,6 @@
     let selectedElementId = ref(null);
     let lockAspectRatio = ref(false);
     let editor = ref(null);
-    let breakpoint = ref(false);
 
     let selectedCategory = ref({});
 
@@ -54,7 +53,6 @@
     function updateSelectedCategory(category) {
         selectedCategory.value = category;
         catalogShow.value = true;
-        fetchCatalogElements(category.name, [], []);
     }
 
     function handleSelectAllAssets() {
@@ -107,14 +105,7 @@
     <div class="editor" ref="editor">
         <div class="editor__top-nav top-nav-lg">
             <div class="top-nav__left-btns">
-                <button
-                    class="top-nav__item back-btn icon icon-btn"
-                    @click="
-                        () => {
-                            goingBackPopUpShow = true;
-                        }
-                    "
-                >
+                <button class="top-nav__item back-btn icon icon-btn" @click="goingBackPopUpShow = true">
                     arrow_back
                 </button>
                 <div class="undo-redo-container d-none">
@@ -127,13 +118,13 @@
                 <div class="top-nav__item layer-btn">
                     <button @click="layersShow = true" class="secondary-btn">
                         <div class="icon">stacks</div>
-                        <span class="display-none">Layers</span>
+                        <span class="d-none d-lg-block">Layers</span>
                     </button>
                 </div>
                 <div class="top-nav__item preview-btn d-none">
                     <button @click="previewShow = true" class="secondary-btn">
                         <div class="icon">preview</div>
-                        <span class="display-none"> Preview </span>
+                        <span class="d-none d-lg-block"> Preview </span>
                     </button>
                 </div>
                 <div class="top-nav__item export-btn">
@@ -145,31 +136,28 @@
                             }"
                         >
                             <div class="icon">download</div>
-                            <span class="display-none"> Download</span>
+                            <span class="d-none d-lg-block"> Download</span>
                         </NuxtLink>
                     </button>
                 </div>
             </div>
         </div>
-        <div class="editor__canvas">
-            <ComicPanels
-                :lockAspectRatio="lockAspectRatio"
-                :comic="comic"
-                @active-panel-change="activePanelIndex = $event"
-            ></ComicPanels>
-        </div>
-
-        <div class="bottom-nav__container">
-            <div class="editor__bottom-nav">
-                <div class="bottom-nav__scrollable-nav">
-                    <CatalogNavigation
-                        :categories="catalogStructure.categories"
-                        @categorySelected="updateSelectedCategory"
-                        @selectAllAssets="handleSelectAllAssets"
-                    />
-                </div>
+        <div class="d-flex flex-column flex-lg-row flex-grow-1">
+            <div class="editor__canvas col-12 col-lg-8">
+                <ComicPanels
+                    :lockAspectRatio="lockAspectRatio"
+                    :comic="comic"
+                    @active-panel-change="activePanelIndex = $event"
+                ></ComicPanels>
             </div>
-            <div class="catalogue-container">
+            <div class="bottom-nav__scrollable-nav col-12 col-lg-2 col-xl-1 order-lg-first">
+                <CatalogNavigation
+                    :categories="catalogStructure.categories"
+                    @categorySelected="updateSelectedCategory"
+                    @selectAllAssets="handleSelectAllAssets"
+                />
+            </div>
+            <div class="catalog-container col-lg-2 col-xl-3 order-lg-first">
                 <CatalogLayout
                     :title="selectedCategory.name"
                     :selectedCategoryAssets="catalogElements"
@@ -178,53 +166,53 @@
                 />
             </div>
         </div>
-        <div class="modal-container">
-            <OverlayModal :full="true" :show="catalogShow" @close="catalogShow = false">
-                <div class="category__description">
-                    <div class="edit-icon icon text-primary">
-                        {{ iconConfig.get(selectedCategory.name) || 'default_icon' }}
-                    </div>
-                    <div class="navigation__title h1">{{ selectedCategory.name }}</div>
+    </div>
+    <div class="d-lg-none">
+        <OverlayModal :full="true" :show="catalogShow" @close="catalogShow = false">
+            <div class="category__description">
+                <div class="edit-icon icon text-primary">
+                    {{ iconConfig.get(selectedCategory.name) || 'default_icon' }}
                 </div>
-                <CatalogLayout
-                    :selectedCategoryAssets="catalogElements"
-                    :selectedCategory="selectedCategory"
-                    @catalog-changed="(e) => fetchCatalogElements(e.category, e.subCategory, e.filter)"
-                />
-            </OverlayModal>
-        </div>
-        <OverlayModal :show="goingBackPopUpShow" :full="false" @close="goingBackPopUpShow = false">
-            <DecisionPopUp
-                imgSrc="http://localhost:3000/catalog/Characters/single/Barista%20pouring4.png?raw=true"
-                title="Poof, Your hard work disappears"
-                body="Are you sure you want to delete your draft? All the changes you've made will be discarded."
-                :buttons="[
-                    { name: 'Save Draft', function: 'save' },
-                    { name: 'Discard all changes', function: 'discard' },
-                    { name: 'Cancel', function: 'cancel' },
-                ]"
-                @cancel="goingBackPopUpShow = false"
-                @save="saveComic"
-                @discard="discardComic"
+                <div class="navigation__title h1">{{ selectedCategory.name }}</div>
+            </div>
+            <CatalogLayout
+                :selectedCategoryAssets="catalogElements"
+                :selectedCategory="selectedCategory"
+                @catalog-changed="(e) => fetchCatalogElements(e.category, e.subCategory, e.filter)"
             />
         </OverlayModal>
-        <ScreenOverlay title="Layers" :show="layersShow" @close="layersShow = false">
-            <div class="layer-background">
-                <div class="layer-container">
-                    <LayerObject
-                        :panel="comic.getPage(0).getStrip(0).getPanel(activePanelIndex)"
-                        @selection-event="selectElement"
-                    >
-                    </LayerObject>
-                </div>
-            </div>
-        </ScreenOverlay>
-        <ScreenOverlay title="Preview" :show="previewShow" @close="previewShow = false">
-            <div class="darken-background">
-                <div class="comic-preview"></div>
-            </div>
-        </ScreenOverlay>
     </div>
+    <OverlayModal :show="goingBackPopUpShow" :full="false" @close="goingBackPopUpShow = false">
+        <DecisionPopUp
+            imgSrc="http://localhost:3000/catalog/Characters/single/Barista%20pouring4.png?raw=true"
+            title="Poof, Your hard work disappears"
+            body="Are you sure you want to delete your draft? All the changes you've made will be discarded."
+            :buttons="[
+                { name: 'Save Draft', function: 'save' },
+                { name: 'Discard all changes', function: 'discard' },
+                { name: 'Cancel', function: 'cancel' },
+            ]"
+            @cancel="goingBackPopUpShow = false"
+            @save="saveComic"
+            @discard="discardComic"
+        />
+    </OverlayModal>
+    <ScreenOverlay title="Layers" :show="layersShow" @close="layersShow = false">
+        <div class="layer-background">
+            <div class="layer-container">
+                <LayerObject
+                    :panel="comic.getPage(0).getStrip(0).getPanel(activePanelIndex)"
+                    @selection-event="selectElement"
+                >
+                </LayerObject>
+            </div>
+        </div>
+    </ScreenOverlay>
+    <ScreenOverlay title="Preview" :show="previewShow" @close="previewShow = false">
+        <div class="darken-background">
+            <div class="comic-preview"></div>
+        </div>
+    </ScreenOverlay>
 </template>
 
 <style scoped lang="scss">
@@ -234,6 +222,16 @@
         visibility: hidden;
     }
 
+    .editor {
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+
+        &__top-nav {
+            justify-content: space-between;
+        }
+    }
+
     .secondary-btn {
         border: none;
         background-color: transparent;
@@ -241,28 +239,27 @@
         column-gap: $spacer-1;
         text-align: center;
         vertical-align: middle;
-    }
 
-    .secondary-btn a {
-        text-decoration: none;
-        color: $white;
-        display: flex;
-        column-gap: $spacer-1;
-    }
+        a {
+            text-decoration: none;
+            color: $white;
+            display: flex;
+            column-gap: $spacer-1;
+        }
 
-    .icon-btn {
-        color: $white;
-    }
-
-    .display-none {
-        display: none;
+        @include media-breakpoint-up(lg) {
+            display: flex;
+            column-gap: $spacer-1;
+            width: 100%;
+            height: 100%;
+            padding: $spacer-3 $spacer-5;
+        }
     }
 
     .layer-background {
         width: 100vw;
         height: 100vh;
         background-color: $white;
-        margin-top: -$spacer-7;
     }
 
     .layer-container {
@@ -272,56 +269,45 @@
         align-items: center;
     }
 
-    .editor__top-nav {
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        background: linear-gradient(90deg, #6360f4 44.5%, #f460b7 100%);
-        height: 80px;
-        margin: 0;
-    }
-
     .top-nav__left-btns {
         display: flex;
         column-gap: $spacer-3;
+
+        @include media-breakpoint-up(lg) {
+            column-gap: $spacer-6;
+        }
     }
 
     .undo-redo-container {
         display: flex;
         column-gap: $spacer-1;
         color: $white;
+
+        @include media-breakpoint-up(lg) {
+            column-gap: $spacer-4;
+        }
     }
 
     .editor__canvas {
         display: flex;
         align-items: center;
-        justify-content: flex-end;
-        height: calc(100vh - 80px);
-        width: 70vw;
-        margin-left: auto;
-    }
-
-    .editor__bottom-nav {
-        background-color: $grey-90;
-        padding: $spacer-3;
+        justify-content: center;
+        flex-grow: 1;
     }
 
     .bottom-nav__scrollable-nav {
         display: flex;
+        background-color: $grey-90;
+        padding: $spacer-3;
         overflow-x: auto;
-    }
+        scroll-behavior: smooth;
 
-    .scrollable-nav__item {
-        padding: $spacer-2 $spacer-3;
-        margin-right: $spacer-2;
-        color: $grey-70;
-        cursor: pointer;
-        border-radius: $border-radius;
-        background-color: $white;
-    }
-
-    .scrollable-nav__item:hover {
-        background-color: $grey-60;
+        @include media-breakpoint-up(lg) {
+            flex-direction: column;
+            gap: $spacer-2;
+            overflow-x: visible;
+            flex-grow: 0;
+        }
     }
 
     .darken-background {
@@ -342,15 +328,12 @@
     }
 
     .bottom-nav__container {
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        background-color: $white;
-        z-index: 1000;
-        overflow-x: scroll;
         white-space: nowrap;
-        scroll-behavior: smooth;
+        display: flex;
+
+        @include media-breakpoint-up(lg) {
+            white-space: wrap;
+        }
     }
 
     .share__top-nav-item {
@@ -358,8 +341,14 @@
         text-decoration: none;
     }
 
-    .catalogue-container {
+    .catalog-container {
         display: none;
+
+        @include media-breakpoint-up(lg) {
+            display: flex;
+            background-color: $white;
+            height: calc(100vh - 80px);
+        }
     }
 
     .category__description {
@@ -367,86 +356,5 @@
         color: $primary;
         display: flex;
         gap: $spacer-2;
-    }
-
-    @include media-breakpoint-up(lg) {
-        .editor:before {
-            content: 'lg';
-        }
-
-        .secondary-btn {
-            display: flex;
-            column-gap: $spacer-1;
-            width: 100%;
-            height: 100%;
-            padding: $spacer-3 $spacer-5;
-        }
-
-        .display-none {
-            display: block;
-        }
-
-        .top-nav__left-btns {
-            display: flex;
-            column-gap: $spacer-6;
-        }
-
-        .undo-redo-container {
-            column-gap: $spacer-4;
-        }
-        .editor__bottom-nav {
-            height: calc(100vh - 80px);
-        }
-
-        .mobile {
-            display: none;
-        }
-
-        .editor__canvas {
-            align-items: center;
-            display: flex;
-            justify-content: right;
-            padding: $spacer-4;
-        }
-
-        .bottom-nav__container {
-            position: absolute;
-            top: 80px;
-            left: 0;
-            width: 200px;
-            background-color: $white;
-            z-index: 1000;
-        }
-
-        .bottom-nav__scrollable-nav {
-            display: flex;
-            flex-direction: column;
-            padding: 20px;
-            gap: 10px;
-        }
-
-        .bottom-nav__scrollable-nav {
-            display: flex;
-            overflow-y: auto;
-        }
-
-        .catalogue-container {
-            display: block;
-            width: 25vw;
-            background-color: $white;
-            position: absolute;
-            top: 0px;
-            left: 200px;
-            z-index: 900;
-            height: calc(100vh - 80px);
-        }
-
-        .modal-container {
-            display: none;
-        }
-        .bottom-nav__container {
-            overflow-x: visible;
-            white-space: wrap;
-        }
     }
 </style>
