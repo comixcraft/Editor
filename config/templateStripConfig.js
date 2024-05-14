f9687234dc5a6e0b53945e367a2e2d3d682a1df2
Vanessa <47862956+Vanessi-K@users.noreply.github.com>
2024-05-14 17:15:47 +0200

    CXC-309: responsive panels (#41)
    * CXC-309: sets height and width of templates to the actual size of the image
    
    * CXC-309: scale individual panels based on available height/width
    
    * CXC-309: add border as background image
    
    * CXC-309: resize Elements on canvas
    
    * CXC-309: correctly set height and width of panel
    
    * CXC-309: resize without glitch
    
    * CXC-309: start width and height of element relative

--- config/templateStripConfig.js
+++ config/templateStripConfig.js
@@ -1,49 +1,46 @@
-// FIXME: The used height and width arenot the actual (highest) resolution of the image.
-//  Needs to be adjusted, when the canvas can be created dynamically.
-
 export default [
     {
         title: 'Three Single',
         preview: '/frames/preview/Preview_Single-Single-Single.png',
         config: {
-            height: 524,
+            height: 1084,
             panels: [
-                { width: 380, border: '/frames/Frame-Single.png' },
-                { width: 380, border: '/frames/Frame-Single.png' },
-                { width: 380, border: '/frames/Frame-Single.png' },
+                { width: 759, border: '/frames/Frame-Single.png' },
+                { width: 759, border: '/frames/Frame-Single.png' },
+                { width: 759, border: '/frames/Frame-Single.png' },
             ],
         },
     },
-    // {
-    //     title: 'Double and Single',
-    //     preview: '/frames/preview/Preview_Double-Single.png',
-    //     config: {
-    //         height: 524,
-    //         panels: [
-    //             { width: 776, border: '/frames/Frame-Double.png' },
-    //             { width: 380, border: '/frames/Frame-Single.png' },
-    //         ],
-    //     },
-    // },
-    // {
-    //     title: 'Single and Double',
-    //     preview: '/frames/preview/Preview_Single-Double.png',
-    //     config: {
-    //         height: 524,
-    //         panels: [
-    //             { width: 380, border: '/frames/Frame-Single.png' },
-    //             { width: 776, border: '/frames/Frame-Double.png' },
-    //         ],
-    //     },
-    // },
+    {
+        title: 'Double and Single',
+        preview: '/frames/preview/Preview_Double-Single.png',
+        config: {
+            height: 1084,
+            panels: [
+                { width: 1552, border: '/frames/Frame-Double.png' },
+                { width: 759, border: '/frames/Frame-Single.png' },
+            ],
+        },
+    },
+    {
+        title: 'Single and Double',
+        preview: '/frames/preview/Preview_Single-Double.png',
+        config: {
+            height: 1084,
+            panels: [
+                { width: 759, border: '/frames/Frame-Single.png' },
+                { width: 1552, border: '/frames/Frame-Double.png' },
+            ],
+        },
+    },
     {
         title: 'Two Single',
         preview: '/frames/preview/Preview_Single-Single.png',
         config: {
-            height: 524,
+            height: 1084,
             panels: [
-                { width: 380, border: '/frames/Frame-Single.png' },
-                { width: 380, border: '/frames/Frame-Single.png' },
+                { width: 759, border: '/frames/Frame-Single.png' },
+                { width: 759, border: '/frames/Frame-Single.png' },
             ],
         },
     },
