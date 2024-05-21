57b297715fb9a6205c3152939419062b778cc917
Anna <cc211018@fhstp.ac.at>
2024-05-21 17:24:56 +0200

    CXC-340 create page and FooterComponent
    create impressum page, start with FooterComponent - desktop design already finished

---
+++ components/FooterComponent.vue
@@ -0,0 +1,55 @@
+<script setup></script>
+
+<template>
+    <footer class="footer">
+        <div class="first-row">
+            <img class="logo" src="/public/comixcraftLogo.svg" />
+            <div class="links">
+                <a>Impressum</a>
+                <a>Legal Information</a>
+                <a>Comixplain</a>
+            </div>
+        </div>
+        <hr class="line" />
+        <p class="copyright">© comixcraft 2024. All rights reserved.</p>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+    .footer {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+        background-color: $primary-30;
+        padding: $spacer-5 $spacer-7;
+
+        .first-row {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+        }
+
+        .links {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            gap: $spacer-3;
+            text-align: right;
+        }
+
+        .logo {
+            width: 15rem;
+        }
+
+        .line {
+            margin: $spacer-3 0;
+        }
+
+        .copyright {
+            text-align: right;
+        }
+    }
+</style>
---
+++ pages/impressum.vue
@@ -0,0 +1,35 @@
+<script setup></script>
+
+<template>
+    <div class="index">
+        <div class="top-nav">
+            <img src="/public/TextwithBg.svg" alt="" class="top-nav__logo" />
+        </div>
+
+        <FooterComponent />
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .top-nav {
+        justify-content: flex-start;
+    }
+
+    .top-nav__logo {
+        display: flex;
+        flex-direction: row;
+        margin-left: $spacer-4;
+        justify-content: center;
+        width: 10rem;
+    }
+
+    @include media-breakpoint-up(lg) {
+        .top-nav {
+            justify-content: flex-start;
+            padding: $spacer-4 $spacer-3;
+        }
+        .top-nav__logo {
+            justify-content: flex-start;
+        }
+    }
+</style>
