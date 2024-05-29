196c6f18e54a57e33052676172873ebd81ca44e8
Vanessa <47862956+Vanessi-K@users.noreply.github.com>
2024-05-29 13:31:21 +0200

    CXC-416: remove unused code-parts

--- pages/editor.vue
+++ pages/editor.vue
@@ -6,7 +6,6 @@
     let previewShow = ref(false);
     let catalogShow = ref(false);
     let goingBackPopUpShow = ref(false);
-    let selectedElementId = ref(null);
     let lockAspectRatio = ref(false);
     let editor = ref(null);
     let userDidSomething = ref(false);
@@ -64,10 +63,6 @@
     function updateSelectedCategory(category) {
         selectedCategory.value = category;
         catalogShow.value = true;
-    }
-
-    function selectElement(eId) {
-        selectedElementId.value = eId;
     }
 
     function saveComic() {
@@ -242,11 +237,7 @@
     <ScreenOverlay title="Layers" :show="layersShow" @close="layersShow = false">
         <div class="layer-background">
             <div class="layer-container">
-                <LayerObject
-                    :panel="comic.getPage(0).getStrip(0).getPanel(activePanelIndex)"
-                    @selection-event="selectElement"
-                >
-                </LayerObject>
+                <LayerObject :panel="comic.getPage(0).getStrip(0).getPanel(activePanelIndex)"> </LayerObject>
             </div>
         </div>
     </ScreenOverlay>
