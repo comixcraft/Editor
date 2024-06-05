aa1bbc7f0f0a62d203535e89a6a24d8e67e56f8f
Alica Holla <alica.holla@gmail.com>
2024-06-05 14:06:19 +0200

    Padding top for .intro

--- pages/index.vue
+++ pages/index.vue
@@ -194,7 +194,7 @@
         flex-direction: row;
         justify-content: space-between;
         align-items: center;
-        padding: 0;
+        padding: $spacer-3 $spacer-2;
     }
 
     .templates {
