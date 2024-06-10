6d69096e12bff534071f7000a7d1f7d099708d84
Anna <cc211018@fhstp.ac.at>
2024-06-10 12:57:49 +0200

    CXC-457 add app from dev to config

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,21 @@ export default defineNuxtConfig({
             },
         },
     },
+    app: {
+        head: {
+            link: [
+                { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+                { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+                { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+                { rel: 'manifest', href: '/site.webmanifest' },
+                { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' },
+            ],
+            meta: [
+                { name: 'msapplication-TileColor', content: '#da532c' },
+                { name: 'theme-color', content: '#ffffff' },
+            ],
+        },
+    },
     gtag: {
         id: 'G-M3GN7L92TF', // Replace with your Google Analytics Measurement ID
         config: {
