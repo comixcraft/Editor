105c08c398147f6fdc29994951478f520440e615
esitsolf <85215120+esitsolf@users.noreply.github.com>
2024-06-19 12:05:55 +0200

    filename of export and draft + Save button (#126)
    * Allow user to give its export a name
    
    an input field allow user to enter a name that is used to name the export file when it is downloaded or shared.
    
    * Add input for nam in editor navbar
    
    An input for the name is now available on the navbar.
    The input is used to change the name of the comic.
    The name is also taken in account on the download page.
    
    * first draft on saving popup with input
    
    * CXC-341 filename export
    
    Added save icon to top nav, added draft name to index page,
    added a text to the save draft/ export popup
    
    * CXC-341
    
    Added logic to save draft btn/ save draft popup, made top nav responsive
    
    * save comic without reload
    
    Save comic doesn't change the page if not needed.
    Small change on title of the comic on index to make it more visible.
    Change the name of the comic is now consider as a modification and will ask for save.
    
    * remove feedback button
    
    * reorder editor
    
    * reorder index
    
    * Solve the bug when loading a draft
    
    ---------
    
    Co-authored-by: Esitsolf <esitsolf@hotmail.fr>
    Co-authored-by: hannahwagner1402 <92428401+hannahwagner1402@users.noreply.github.com>

--- pages/editor.vue
+++ pages/editor.vue
@@ -1,55 +1,96 @@
 <script setup>
+    // Imports
     import ComicPanels from '~/components/ComicPanels.vue';
     import iconConfig from '../config/iconsConfig';
 
-    let layersShow = ref(false);
-    let previewShow = ref(false);
-    let catalogShow = ref(false);
-    let goingBackPopUpShow = ref(false);
-    let lockAspectRatio = ref(false);
-    let editor = ref(null);
-    let refreshCount = ref(0);
-    let intersectionObserver;
-    let popUpText = ref('');
-    let selectedCategory = ref({});
+    // Middlewares
+    definePageMeta({
+        middleware: ['comic-defined'],
+    });
 
-    const undoEmpty = computed(() => {
-        return comicStore.comic.getPage(0).getStrip(0).panels[activePanelIndex.value].cantUndo;
+    // Emits
+
+    // Props
+
+    // Static Variables (let, const)
+    const allAssetsCategoryName = 'All Assets';
+    const comicStore = useComicStore();
+    const router = useRouter();
+
+    let intersectionObserver;
+
+    // Reactive Variables
+    // computed
+    const bottomNavHeight = computed(() => {
+        return `${Math.floor(navReactiveHeight.value * 10) / 10}px`;
     });
 
     const redoEmpty = computed(() => {
         return comicStore.comic.getPage(0).getStrip(0).panels[activePanelIndex.value].cantRedo;
     });
 
+    const undoEmpty = computed(() => {
+        return comicStore.comic.getPage(0).getStrip(0).panels[activePanelIndex.value].cantUndo;
+    });
+
     const userDidSomething = computed(() => {
         return comicStore.userDidSomething;
     });
 
-    definePageMeta({
-        middleware: ['comic-defined'],
-    });
+    // Reactive
+    const comic = reactive(toRaw(comicStore.comic));
 
-    const allAssetsCategoryName = 'All Assets';
-    const comicStore = useComicStore();
+    // Refs
+    const activePanelIndex = ref(0);
     const catalogElements = ref(null);
     const catalogStructure = ref([]);
-    const comic = reactive(toRaw(comicStore.comic));
-    const activePanelIndex = ref(0);
-    const scrollableNav = ref(null);
     const isScrollableLeft = ref(false);
     const isScrollableRight = ref(true);
     const navReactiveHeight = ref(undefined);
-    await useFetch('/api/catalog/structure')
-        .then((response) => {
-            catalogStructure.value = response.data.value;
-        })
-        .catch((error) => {
-            createError(error);
-        });
+    const scrollableNav = ref(null);
+    let catalogShow = ref(false);
+    let editor = ref(null);
+    let goingBackPopUpShow = ref(false);
+    let layersShow = ref(false);
+    let mobileMenu = ref(false);
+    let popUpText = ref('');
+    let previewShow = ref(false);
+    let refreshCount = ref(0);
+    let saveDraftPopUpShow = ref(false);
+    let selectedCategory = ref({});
 
-    let bottomNavHeight = computed(() => {
-        return `${Math.floor(navReactiveHeight.value * 10) / 10}px`;
-    });
+    // Watchers
+    watch(
+        () => comic.getPage(0).getStrip(0).getPanel(0).elements,
+        () => comicStore.setUserDidSomething(true),
+        { deep: true }
+    );
+
+    watch(
+        () => comic.name,
+        () => comicStore.setUserDidSomething(true),
+        { deep: true }
+    );
+
+    // Methods
+    function changeScrollingBooleans(element, bool) {
+        element === scrollableNav.value.firstChild.firstChild
+            ? (isScrollableLeft.value = bool)
+            : (isScrollableRight.value = bool);
+    }
+
+    function detectScrollingPosition(entries) {
+        entries.forEach((entry) => {
+            entry.isIntersecting
+                ? changeScrollingBooleans(entry.target, false)
+                : changeScrollingBooleans(entry.target, true);
+        });
+    }
+
+    function discardComic() {
+        comicStore.setUserDidSomething(false);
+        reloadApp();
+    }
 
     function fetchCatalogElements(category = [], subCategory = [], filter = []) {
         if (category === allAssetsCategoryName) category = [];
@@ -72,41 +113,6 @@
             });
     }
 
-    function updateSelectedCategory(category) {
-        selectedCategory.value = category;
-        catalogShow.value = true;
-    }
-
-    function saveComic() {
-        let comicJson = comicStore.comic.toJSON();
-        comicStore.saveDraft(comicJson);
-
-        comicStore.setComingBackAfterSaving(true);
-
-        return reloadNuxtApp({
-            path: '/',
-            ttl: 1000,
-        });
-    }
-
-    function discardComic() {
-        comicStore.setUserDidSomething(false);
-        return reloadNuxtApp({
-            path: '/',
-            ttl: 1000,
-        });
-    }
-
-    function handleUndo() {
-        comicStore.comic.getPage(0).getStrip(0).panels[activePanelIndex.value].undo();
-        refreshCount.value++;
-    }
-
-    function handleRedo() {
-        comicStore.comic.getPage(0).getStrip(0).panels[activePanelIndex.value].redoAction();
-        refreshCount.value++;
-    }
-
     function handleGoingBack() {
         if (userDidSomething.value) {
             goingBackPopUpShow.value = true;
@@ -118,13 +124,60 @@
             }
         } else {
             comicStore.setUserDidSomething(false);
-            return reloadNuxtApp({
-                path: '/',
-                ttl: 1000,
-            });
+            reloadApp();
         }
     }
 
+    function handleRedo() {
+        comicStore.comic.getPage(0).getStrip(0).panels[activePanelIndex.value].redoAction();
+        refreshCount.value++;
+    }
+
+    function handleSaveDraftBtn() {
+        if (comicStore.getDraft().value !== null) {
+            saveDraftPopUpShow.value = true;
+            popUpText.value =
+                'Do you want to save your progress as a draft? <br /> <strong>You can only have one draft at a time.</strong><br /> Saving a new one overwrites the existing one.';
+        } else {
+            saveComic();
+        }
+
+        mobileMenu.value = false;
+    }
+
+    function handleUndo() {
+        comicStore.comic.getPage(0).getStrip(0).panels[activePanelIndex.value].undo();
+        refreshCount.value++;
+    }
+
+    function reloadApp() {
+        return reloadNuxtApp({
+            path: '/',
+            ttl: 1000,
+        });
+    }
+
+    function saveComic(reload = false) {
+        let comicJson = comicStore.comic.toJSON();
+        comicStore.saveDraft(comicJson);
+
+        if (reload) {
+            comicStore.setComingBackAfterSaving(true);
+            reloadApp();
+        } else {
+            comicStore.setUserDidSomething(false);
+            generateToast('success', 'Comic was saved as a draft.');
+        }
+    }
+
+    function updateSelectedCategory(category) {
+        selectedCategory.value = category;
+        catalogShow.value = true;
+    }
+
+    // Bus Listeners
+
+    // Vue life cycle hooks
     window.onbeforeunload = function (e) {
         if (userDidSomething.value && e.target.activeElement === this.document.body) {
             e.preventDefault();
@@ -144,26 +197,6 @@
     window.onresize = function (e) {
         navReactiveHeight.value = scrollableNav.value.getBoundingClientRect().height;
     };
-
-    watch(
-        () => comic.getPage(0).getStrip(0).getPanel(0).elements,
-        () => comicStore.setUserDidSomething(true),
-        { deep: true }
-    );
-
-    function detectScrollingPosition(entries) {
-        entries.forEach((entry) => {
-            entry.isIntersecting
-                ? changeScrollingBooleans(entry.target, false)
-                : changeScrollingBooleans(entry.target, true);
-        });
-    }
-
-    function changeScrollingBooleans(element, bool) {
-        element === scrollableNav.value.firstChild.firstChild
-            ? (isScrollableLeft.value = bool)
-            : (isScrollableRight.value = bool);
-    }
 
     onMounted(() => {
         intersectionObserver = new IntersectionObserver(detectScrollingPosition, {
@@ -186,11 +219,23 @@
         window.onresize = null;
         intersectionObserver.disconnect();
     });
+
+    // expose (defineExpose)
+
+    //
+    await useFetch('/api/catalog/structure')
+        .then((response) => {
+            catalogStructure.value = response.data.value;
+        })
+        .catch((error) => {
+            createError(error);
+        });
 </script>
 
 <template>
     <div class="editor" ref="editor">
         <div class="editor__top-nav top-nav">
+            <!-- Left nav -->
             <div class="top-nav__left-btns">
                 <button class="share__top-nav-item back-btn icon icon-btn" @click="handleGoingBack">arrow_back</button>
                 <div class="undo-redo-container">
@@ -201,22 +246,34 @@
                         Redo
                     </button>
                 </div>
+                <input type="text" class="top-nav__name-input input d-none d-lg-block" v-model="comic.name" />
             </div>
 
+            <!-- Right nav -->
             <div class="top-nav__left-btns">
+                <!-- layer -->
                 <div class="top-nav__item layer-btn">
                     <button @click="layersShow = true" class="secondary-btn">
                         <div class="icon">stacks</div>
-                        <span class="d-none d-lg-block">Layers</span>
+                        <span class="d-none d-xl-block">Layers</span>
                     </button>
                 </div>
+                <!-- preview btn -->
                 <div class="top-nav__item preview-btn">
                     <button @click="previewShow = true" class="secondary-btn">
                         <div class="icon">preview</div>
-                        <span class="d-none d-lg-block"> Preview </span>
+                        <span class="d-none d-xl-block"> Preview </span>
                     </button>
                 </div>
-                <div class="top-nav__item export-btn">
+                <!-- save draft btn -->
+                <div class="top-nav__item saveDraft-btn d-none d-lg-block">
+                    <button class="secondary-btn" @click="handleSaveDraftBtn">
+                        <div class="icon">save</div>
+                        <span class="d-none d-xl-block"> Save </span>
+                    </button>
+                </div>
+                <!-- download btn -->
+                <div class="top-nav__item export-btn d-none d-lg-block">
                     <button class="secondary-btn">
                         <NuxtLink
                             :to="{
@@ -225,12 +282,19 @@
                             }"
                         >
                             <div class="icon">download</div>
-                            <span class="d-none d-lg-block"> Download</span>
+                            <span class="d-none d-xl-block"> Download</span>
                         </NuxtLink>
                     </button>
                 </div>
+
+                <!-- mobile menu -->
+                <div class="top-nav__item export-btn d-lg-none">
+                    <button class="secondary-btn icon" @click="mobileMenu = true">more_horiz</button>
+                </div>
             </div>
         </div>
+
+        <!-- Editor -->
         <div class="d-flex flex-column flex-lg-row flex-grow-1">
             <div class="editor__canvas col-12 col-lg-8">
                 <ComicPanels
@@ -240,6 +304,7 @@
                     @active-panel-change="activePanelIndex = $event"
                 ></ComicPanels>
             </div>
+            <!-- Catalog -->
             <div
                 class="bottom-nav__scrollable-nav col-12 col-lg-2 col-xl-1 order-lg-first"
                 :class="{
@@ -263,6 +328,8 @@
             </div>
         </div>
     </div>
+
+    <!-- Catalog nav on mobile -->
     <div class="d-lg-none">
         <OverlayModal :full="true" :show="catalogShow" @close="catalogShow = false" :padding="'0'">
             <div class="category__description">
@@ -281,6 +348,8 @@
             </div>
         </OverlayModal>
     </div>
+
+    <!-- Going back popup -->
     <OverlayModal :show="goingBackPopUpShow" :full="false" @close="goingBackPopUpShow = false">
         <DecisionPopUp
             imgSrc="/Barista Exclaiming4.png"
@@ -291,12 +360,48 @@
                 { name: 'Return to Editing', emitName: 'cancel' },
             ]"
             @cancel="goingBackPopUpShow = false"
-            @save="saveComic"
+            @save="saveComic(true)"
             @discard="discardComic"
         >
             <div v-html="popUpText"></div>
         </DecisionPopUp>
     </OverlayModal>
+
+    <!-- saveDraft Popup -->
+    <OverlayModal :show="saveDraftPopUpShow" :full="false" @close="saveDraftPopUpShow = false" class="save-popup">
+        <DecisionPopUp
+            imgSrc="/Barista Exclaiming4.png"
+            title="You can only have one draft."
+            :buttons="[
+                { name: 'Save Draft', emitName: 'save' },
+                { name: 'Cancel', emitName: 'cancel' },
+            ]"
+            @cancel="saveDraftPopUpShow = false"
+            @save="saveComic(), (saveDraftPopUpShow = false)"
+        >
+            <div v-html="popUpText"></div>
+        </DecisionPopUp>
+    </OverlayModal>
+
+    <!-- mobile submenu popup -->
+    <OverlayModal :show="mobileMenu" :full="false" @close="mobileMenu = false">
+        <DecisionPopUp
+            :buttons="[
+                { name: 'Export', emitName: 'download' },
+                { name: 'Save Draft', emitName: 'save' },
+            ]"
+            @save="handleSaveDraftBtn"
+            @download="router.push({ name: 'export', path: '/export' })"
+        >
+            <div class="project-name">
+                <label for="project-name">Project Name</label>
+                <input type="text" id="project-name" class="input" v-model="comic.name" />
+            </div>
+        </DecisionPopUp>
+        <p class="alertDraftTxt">Saving a draft will override any currently saved draft</p>
+    </OverlayModal>
+
+    <!-- Layers overlay -->
     <ScreenOverlay title="Layers" :show="layersShow" @close="layersShow = false">
         <div class="layer-background">
             <div class="layer-container">
@@ -304,6 +409,8 @@
             </div>
         </div>
     </ScreenOverlay>
+
+    <!-- Preview overlay -->
     <ScreenOverlay
         title="Preview"
         :show="previewShow"
@@ -367,7 +474,7 @@
             column-gap: $spacer-1;
             width: 100%;
             height: 100%;
-            padding: $spacer-3 $spacer-5;
+            padding: $spacer-3 $spacer-3;
             &:hover {
                 scale: 1.1;
             }
@@ -386,15 +493,6 @@
         justify-content: center;
         align-items: center;
         background-color: $white;
-    }
-
-    .top-nav__left-btns {
-        display: flex;
-        column-gap: $spacer-3;
-
-        @include media-breakpoint-up(lg) {
-            column-gap: $spacer-6;
-        }
     }
 
     .undo-redo-container {
@@ -489,6 +587,13 @@
         }
     }
 
+    .alertDraftTxt {
+        font-style: italic;
+        align-items: center;
+        text-align: center;
+        padding-top: $spacer-5;
+    }
+
     .catalog-container {
         display: none;
 
@@ -518,35 +623,37 @@
         overflow-y: auto;
     }
 
-    .top-nav__item-undo-btn {
-        color: $grey-0;
-        @include media-breakpoint-up(lg) {
-            &:hover {
-                scale: 1.2;
-            }
-            &:disabled {
-                scale: 1; // Prevent scaling on hover when disabled
-                cursor: not-allowed; // Change cursor to indicate disabled state
+    .top-nav {
+        &__item-undo-btn,
+        &__item-redo-btn {
+            color: $grey-0;
+            @include media-breakpoint-up(lg) {
                 &:hover {
-                    scale: 1;
+                    scale: 1.2;
+                }
+                &:disabled {
+                    scale: 1; // Prevent scaling on hover when disabled
+                    cursor: not-allowed; // Change cursor to indicate disabled state
+                    &:hover {
+                        scale: 1;
+                    }
                 }
             }
         }
-    }
+        &__left-btns {
+            display: flex;
+            column-gap: $spacer-3;
+            align-items: center;
 
-    .top-nav__item-redo-btn {
-        color: $grey-0;
-        @include media-breakpoint-up(lg) {
-            &:hover {
-                scale: 1.2;
+            @include media-breakpoint-up(lg) {
+                column-gap: $spacer-5;
             }
-            &:disabled {
-                scale: 1;
-                cursor: not-allowed;
-                &:hover {
-                    scale: 1;
-                }
-            }
+        }
+        &__name-input {
+            border: 1px solid $white;
+            background-color: transparent;
+            color: $white;
+            padding: $spacer-2;
         }
     }
 
@@ -554,5 +661,23 @@
         .edit-icon {
             font-size: 40px;
         }
+    }
+
+    .project-name {
+        display: flex;
+        flex-direction: column;
+        border-bottom: 1px solid $grey-60;
+        padding-bottom: $spacer-3;
+        align-items: baseline;
+        width: 100%;
+        gap: $spacer-2;
+
+        .input {
+            width: 100%;
+        }
+    }
+
+    .save-popup {
+        z-index: 10000;
     }
 </style>
