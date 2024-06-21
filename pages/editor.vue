2ff6f9a99f4c939cdb47cafb195ffe26a52786e0
Vanessa <47862956+Vanessi-K@users.noreply.github.com>
2024-06-21 10:05:14 +0200

    CXC-474: use cmd as the modifier key on Macs

--- pages/editor.vue
+++ pages/editor.vue
@@ -176,7 +176,7 @@
     }
 
     function initGlobalKeyboardShortcuts(e) {
-        if (e.ctrlKey && e.key === 's') {
+        if ((e.ctrlKey || e.metaKey) && e.key === 's') {
             e.preventDefault();
             saveComic();
         }
